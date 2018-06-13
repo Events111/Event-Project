1f91be28fc75c54db817eac108ac707ca00a8d6c
WZ <WZ@WZ-PC>
2018-06-13 23:49:22 +0800

    hello world

---
+++ src/com/events/test/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.events.test;
+
+/**
+ * @author WZ
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("test");
+	}
+
+}
